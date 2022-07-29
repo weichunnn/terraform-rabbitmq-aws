0f6a6d37ce8ff81864b344c7714034dd003e3097
weichunnn <tanweichun@outlook.com>
2022-07-29 07:13:55 +0000

    init infra commit

---
+++ terraform/rmq/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+  profile = var.profile 
+}
+
+resource "aws_instance" "rmq" {
+  ami = "ami-07ebfd5b3428b6f4d"  
+  instance_type = "t2.micro"
+  key_name = "devops"
+  vpc_security_group_ids = ["sg-0b996912fe602cd67"]
+
+  tags = {
+    Name = var.name
+    group = var.group
+  }
+}
---
+++ terraform/rmq/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  description = "Name of instance on deploy"
+}
+
+variable "group" {
+  description = "group tag for ansible to identify RMQ"
+}
+
+variable "profile" {
+  description = "profile for deployment"
+}
