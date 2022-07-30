b3d1e42fa87e41bc75c02b0a8bf6882bdbde6b42
weichunnn <tanweichun@outlook.com>
2022-07-30 11:45:38 +0000

    added change from aws

--- terraform/rmq/main.tf
+++ terraform/rmq/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "rmq" {
-  ami = "ami-07ebfd5b3428b6f4d"  
+  ami = "ami-02248c9d6ba0b9e12"  
   instance_type = "t2.micro"
   key_name = "devops"
   vpc_security_group_ids = ["sg-0b996912fe602cd67"]
